e23d4af984dadfceb3260cb7bbb84f3c9f099b4f
Christoph Grenz <christophg+gitorious@grenz-bonn.de>
2013-10-26 23:23:56 +0200

    augmented setup.py

--- setup.py
+++ setup.py
@@ -7,20 +7,20 @@ import os
 
 '''
 	(c) 2012 Christoph Grenz <christophg@grenz-bonn.de>
-	This file is part of python-osm.
+	This file is part of python-hiddev.
 
 	python-hiddev is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
 	the Free Software Foundation, either version 3 of the License, or
 	(at your option) any later version.
 
-	python-osm is distributed in the hope that it will be useful,
+	python-hiddev is distributed in the hope that it will be useful,
 	but WITHOUT ANY WARRANTY; without even the implied warranty of
 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 	GNU General Public License for more details.
 
 	You should have received a copy of the GNU General Public License
-	along with python-osm.  If not, see <http://www.gnu.org/licenses/>.
+	along with python-hiddev.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
 setup(
@@ -30,6 +30,17 @@ setup(
 	author='Christoph Grenz',
 	author_email='christophg+python@grenz-bonn.de',
 	url='https://gitorious.org/python-hiddev/',
+	classifiers=[
+		'Development Status :: 4 - Beta',
+		'Environment :: Console',
+		'Environment :: X11 Applications :: Qt',
+		'Intended Audience :: Developers',
+		'Intended Audience :: System Administrators',
+		'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+		'Operating System :: POSIX :: Linux',
+		'Programming Language :: Python',
+		'Topic :: System :: Hardware :: Hardware Drivers',
+	],
 	packages = ['hiddev'],
 	scripts = ['hiddevexplorer']
 )
